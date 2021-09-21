25b5cc76d85f7fbff3ed4a04e1b5610a37f34b79
Ramon Rodrigues <rwxramon@gmail.com>
2021-09-21 04:23:32 -0300

    add stack

---
+++ stack/stack.go
@@ -0,0 +1,19 @@
+package stack
+
+type Stack struct {
+	items []int
+}
+
+// Push will add value at the end
+func (s *Stack) Push(i int) {
+	s.items = append(s.items, i)
+}
+
+// Pop will remove a value at the end
+// and RETURNS the removed value
+func (s *Stack) Pop() int {
+	l := len(s.items) - 1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
