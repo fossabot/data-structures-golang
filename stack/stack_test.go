1e46d0809351b5b47d22bdcd0ccaaadd55ecdad4
Ramon Rodrigues <rwxramon@gmail.com>
2021-09-21 13:29:12 -0300

    naming the test cases of the Pop function

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -33,22 +33,33 @@ func TestPush(t *testing.T) {
 
 func TestPop(t *testing.T) {
 	var testCases = []struct {
-		stack Stack
-		want  Stack
+		name   string
+		stack  Stack
+		want   Stack
+		popVal int
 	}{
 		{
+			"Remove the value 300 from the stack",
 			Stack{[]int{100, 200, 300}},
 			Stack{[]int{100, 200}},
+			300,
 		},
 		{
+			"Remove the value 30 from the stack",
 			Stack{[]int{10, 20, 30}},
 			Stack{[]int{10, 20}},
+			30,
 		},
 	}
 	for _, tc := range testCases {
-		tc.stack.Pop()
-		if !reflect.DeepEqual(tc.stack, tc.want) {
-			t.Error("expected:", tc.want, "got:", tc.stack)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			pop := tc.stack.Pop()
+			if !reflect.DeepEqual(tc.stack, tc.want) {
+				t.Error("expected:", tc.want, "got:", tc.stack)
+			}
+			if pop != tc.popVal {
+				t.Error("expected:", tc.popVal, "got:", pop)
+			}
+		})
 	}
 }
