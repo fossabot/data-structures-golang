f4c49ce55414df457c2a65925534bf4ef195fa09
Ramon Rodrigues <rwxramon@gmail.com>
2021-09-21 12:50:26 -0300

    add Pop() test

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -30,3 +30,25 @@ func TestPush(t *testing.T) {
 		}
 	}
 }
+
+func TestPop(t *testing.T) {
+	var testCases = []struct {
+		stack Stack
+		want  Stack
+	}{
+		{
+			Stack{[]int{100, 200, 300}},
+			Stack{[]int{100, 200}},
+		},
+		{
+			Stack{[]int{10, 20, 30}},
+			Stack{[]int{10, 20}},
+		},
+	}
+	for _, tc := range testCases {
+		tc.stack.Pop()
+		if !reflect.DeepEqual(tc.stack, tc.want) {
+			t.Error("expected:", tc.want, "got:", tc.stack)
+		}
+	}
+}
