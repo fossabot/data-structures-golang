e12e12a203bd39aa37e95e03ae014a90debab0e9
Ramon Rodrigues <rwxramon@gmail.com>
2021-09-21 19:29:50 -0300

    rewrite doc stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,5 +1,9 @@
+// Stack is a linear data structure which follows a particular order in which
+// the operations are performed. The order may be LIFO(Last In First Out) or
+// FILO(First In Last Out).
 package stack
 
+// Stack represents a queue that holds a slice
 type Stack struct {
 	items []int
 }
@@ -9,8 +13,7 @@ func (s *Stack) Push(i int) {
 	s.items = append(s.items, i)
 }
 
-// Pop will remove a value at the end
-// and RETURNS the removed value
+// Pop will remove a value at the end and return the removed value
 func (s *Stack) Pop() int {
 	l := len(s.items) - 1
 	toRemove := s.items[l]
@@ -18,6 +21,7 @@ func (s *Stack) Pop() int {
 	return toRemove
 }
 
+// Create a new Stack
 func NewStack() Stack {
 	return Stack{}
 }
